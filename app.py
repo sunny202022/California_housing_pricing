26f1b53cc9603caad2b48d9e401978d8b868d92c
Sunny Agarwal <agarwal11sunny@gmail.com>
2024-07-10 22:45:29 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 app=Flask(__name__)
 # load model
 remodel=pickle.load(open('remodel.pkl','rb'))
-scaler=pickle.load(open('scaling.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -18,10 +18,17 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output=remodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=remodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price predicted price is {}".format(output))
 if __name__=="__main__":
     app.run(debug=True)
